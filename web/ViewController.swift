d84e1c674da7f65639ccde8021b0de6e5c546a8d
Erkan Kavas <erkankavas@gmail.com>
2019-03-15 18:26:08 +0300

    ilk sürüm

---
+++ web/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  web
+//
+//  Created by Erkan Kavas on 15.03.2019.
+//  Copyright © 2019 Erkan Kavas. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let hey =  "https://www.r10.net"
+        
+        webView.load(URLRequest(url: URL(string: hey)!))
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
